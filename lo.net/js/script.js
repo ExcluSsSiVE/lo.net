36c29ec508efdfe5067912d98b885dba8cd873f1
Evgeniy Nevgad <Nudny@ukr.net>
2019-06-29 22:23:50 +0300

    Add files via upload

---
+++ lo.net/js/script.js
@@ -0,0 +1,31 @@
+
+/*
+$(document).ready(function(){
+alert('Привет Евгений!');
+});
+*/
+
+
+/*slayd*/
+var slideWidth=930;
+var sliderTimer;
+$(function(){
+$('.slidewrapper').width($('.slidewrapper').children().size()*slideWidth);
+    sliderTimer=setInterval(nextSlide,3000);
+    $('.viewport').hover(function(){
+        clearInterval(sliderTimer);
+    },function(){
+        sliderTimer=setInterval(nextSlide,3000);
+    });
+});
+ 
+function nextSlide(){
+    var currentSlide=parseInt($('.slidewrapper').data('current'));
+    currentSlide++;
+    if(currentSlide>=$('.slidewrapper').children().size())
+    {
+        currentSlide=0;   
+    }
+    $('.slidewrapper').animate({left: -currentSlide*slideWidth},930).data('current',currentSlide);
+}
+/*slayd*/
